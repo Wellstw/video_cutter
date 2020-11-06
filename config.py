28c43d7ef5c95f3d5ef6a00a859e3da8d9b9d2e6
Wells Wang <33578357+Wellstw@users.noreply.github.com>
2020-11-07 00:04:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  6 17:26:14 2020
+
+@author: Soft
+"""
+
+
+ffmpeg_bin="C:\\portableApp\\ffmpeg\\bin\\ffmpeg.exe"
+ffmpeg_audio_extract="-vn -ac 1 -c:a pcm_s16le -ar 16000 "
